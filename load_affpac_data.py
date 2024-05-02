b4da9e14bbae528a1c669f0fe977baeb9d2a4b6b
em-ert <emsterskate@outlook.com>
2024-05-02 09:39:53 -0400

    Added button press epoch function.

--- load_affpac_data.py
+++ load_affpac_data.py
@@ -21,7 +21,7 @@ def load_affpac_data(subject_id=0, data_path = 'data/'):
     else: data = loadmat.loadmat(f'{data_path}/S{subject_id}')
     # print(data.keys())
     # quit()
-    return data['chann'],data['I'],data['X'],data['Y']
+    return data['chann'], data['I'], data['X']*1e6, data['Y']
 
 def plot_raw_data(subject, eeg_data, Y_data, information_array, channels, channels_to_plot,start_stop_time=[0,-1]):
     
@@ -31,14 +31,14 @@ def plot_raw_data(subject, eeg_data, Y_data, information_array, channels, channe
     start_index=int(start_stop_time[0]/sampling_period) #compute integer index for start time
     stop_index=int(start_stop_time[1]/sampling_period) #compute integer index for start time
 
-    fig, axs = plt.subplots(2,sharex=True)
+    fig, axs = plt.subplots(2, sharex=True)
     fig.suptitle(f'AffPac Subject {subject} Raw Data')
     
-
     axs[0].plot(eeg_time[start_index:stop_index],np.squeeze(Y_data[start_index:stop_index]),label='Marker Events')
     axs[0].set_ylabel('Marker Event')
     axs[0].set_xlabel('Time (s)')
     axs[0].grid()
+
         
     #PLot EEG Data
     for channel_index, channel_member in enumerate(channels_to_plot):
@@ -50,11 +50,12 @@ def plot_raw_data(subject, eeg_data, Y_data, information_array, channels, channe
     axs[1].set_xlabel('Time (s)')
     axs[1].legend()
     axs[1].grid()
+    plt.xlim(start_stop_time)
     plt.tight_layout()
     plt.show()
     
     
-def epoch_eeg_data(eeg_data,Y_data):
+def epoch_eeg_data(eeg_data, Y_data, information_array):
     
     #We want to epoch the data for two particular events: Normal (Code 22/23)
     #and frustration (Code 24/25)
@@ -70,7 +71,7 @@ def epoch_eeg_data(eeg_data,Y_data):
     if frustrated_epoch_index_start.shape > frustrated_epoch_index_end.shape: 
         frustrated_epoch_index_start = frustrated_epoch_index_start[:-1]
 
-    #Determine how many trials there is
+    # Determine how many trials there are
     epoch_count_normal = (Y_data == 22).sum()
     epoch_count_frustrated = (Y_data == 24).sum()
 
@@ -79,34 +80,116 @@ def epoch_eeg_data(eeg_data,Y_data):
 
     eeg_epoch_normal = np.full((epoch_count_normal, channels_count, max_event_length), np.nan)
     eeg_epoch_frustrated = np.full((epoch_count_frustrated, channels_count, max_event_length), np.nan)
-
+    
+    # Create empty boolean masks for each epoch
+    normal_epoch_masks = np.zeros((epoch_count_normal, eeg_data.shape[1]))
+    frustrated_epoch_masks = np.zeros((epoch_count_frustrated, eeg_data.shape[1]))
+    
     for epoch_index in range(epoch_count_normal-1):
-        curr_epoch_length = normal_epoch_index_end[epoch_index] - normal_epoch_index_start[epoch_index]
-        eeg_epoch_normal[epoch_index, :,:curr_epoch_length]= eeg_data[:,normal_epoch_index_start[epoch_index]:normal_epoch_index_end[epoch_index]]
+        # Get epoch start and end indices (for code clarity)
+        epoch_start_index = normal_epoch_index_start[epoch_index]
+        epoch_end_index = normal_epoch_index_end[epoch_index]
+        # Find epoch length and add eeg data to the array for the current index
+        curr_epoch_length = epoch_end_index - epoch_start_index
+        eeg_epoch_normal[epoch_index, :, :curr_epoch_length] = eeg_data[ :, epoch_start_index : epoch_end_index]
+        # Fill epoch mask with 1s where this epoch occurred
+        normal_epoch_masks[epoch_index, epoch_start_index : epoch_end_index] = 1
 
     for epoch_index in range(epoch_count_frustrated-1):
-        curr_epoch_length = frustrated_epoch_index_end[epoch_index] - frustrated_epoch_index_start[epoch_index]
-        eeg_epoch_frustrated[epoch_index, :,:curr_epoch_length] = eeg_data[:,frustrated_epoch_index_start[epoch_index]:frustrated_epoch_index_end[epoch_index]]
+        # Get epoch start and end indices (for code clarity)
+        epoch_start_index = frustrated_epoch_index_start[epoch_index]
+        epoch_end_index = frustrated_epoch_index_end[epoch_index]
+        # Find epoch length and add eeg data to the array for the current index
+        curr_epoch_length = epoch_end_index - epoch_start_index
+        eeg_epoch_frustrated[epoch_index, :, :curr_epoch_length] = eeg_data[ :, epoch_start_index : epoch_end_index]
+        # Fill epoch mask with 1s where this epoch occurred
+        frustrated_epoch_masks[epoch_index, epoch_start_index : epoch_end_index] = 1
         
-    return eeg_epoch_normal, eeg_epoch_frustrated
+    epoch_time_array = information_array[0, 0 : max_event_length]
+    
+    return eeg_epoch_normal, eeg_epoch_frustrated, np.array(normal_epoch_masks, dtype=bool), np.array(frustrated_epoch_masks, dtype=bool), epoch_time_array
 
 
-def plot_epoch_data(eeg_epoch_normal, eeg_epoch_frustrated, channels, subject, channel_to_plot): 
+def plot_epoch_data(eeg_epoch_normal, eeg_epoch_frustrated, epoch_time_array, channels, subject, channel_to_plot): 
+    plt.figure()
+    
     channel_index = np.where(channels==channel_to_plot)[0][0]
     for trial in range(eeg_epoch_normal.shape[0]):
         if trial==0: 
-            plt.plot(eeg_epoch_normal[trial,channel_index,:], color = 'blue', label = 'normal')
+            plt.plot(epoch_time_array, eeg_epoch_normal[trial,channel_index,:], color = 'blue', label = 'normal')
         else:
-            plt.plot(eeg_epoch_normal[trial,channel_index,:], color = 'blue')
+            plt.plot(epoch_time_array, eeg_epoch_normal[trial,channel_index,:], color = 'blue')
 
     for trial in range(eeg_epoch_frustrated.shape[0]):
         if trial==0: 
-            plt.plot(eeg_epoch_frustrated[trial,channel_index,:], color = 'red', label = 'frustrated')
+            plt.plot(epoch_time_array, eeg_epoch_frustrated[trial,channel_index,:], color = 'red', label = 'frustrated')
         else: 
-            plt.plot(eeg_epoch_frustrated[trial,channel_index,:], color = 'red')
+            plt.plot(epoch_time_array, eeg_epoch_frustrated[trial,channel_index,:], color = 'red')
 
     plt.legend()
     channel = ''.join(channel_to_plot)
     plt.title(f"Channel {channel} Subject {subject}")
-    plt.savefig(f'plots/subject{subject}_channel{channel}')
-    plt.close()
+    plt.savefig(f'plots/subject-{subject}_channel-{channel}')
+    plt.show()
+    
+
+def plot_after_button_press(eeg_epoch_normal, eeg_epoch_frustrated, normal_epoch_masks, frustrated_epoch_masks, epoch_time_array, Y_data, channels, subject, channel_to_plot, start_stop_time=[-1, 2], presses="first", separate_by_side=False):
+    plt.figure()
+    
+    sampling_period=epoch_time_array[1]-epoch_time_array[0]
+    fs=1/(sampling_period)
+    length_s = abs(start_stop_time[1] - start_stop_time[0])
+    stop_index=int(length_s/sampling_period)
+    button_time_array = epoch_time_array[0:stop_index]
+    channel_index = np.where(channels==channel_to_plot)[0][0]
+    
+    for trial in range(eeg_epoch_normal.shape[0] - 1):
+        # Isolate the right subset of the Y_data
+        trial_Y_data = Y_data[normal_epoch_masks[trial]]
+        
+        # Find time of button presses
+        button_presses = np.array(np.where(trial_Y_data==1) or np.where(trial_Y_data==2) or np.where(trial_Y_data==3) or np.where(trial_Y_data==4))
+        button_presses = button_presses.flatten()
+        
+        start_index=int(start_stop_time[0]/sampling_period) + button_presses[0] #compute integer index for start time
+        stop_index=int(start_stop_time[1]/sampling_period) + button_presses[0] #compute integer index for start time
+        time_array = epoch_time_array[start_index:stop_index]
+        eeg_data = eeg_epoch_normal[trial,channel_index,start_index:stop_index]
+        
+        if start_index < 0:
+            stop_index += abs(start_index)
+            start_index = 0
+        
+        if trial==0:
+            plt.plot(button_time_array, eeg_epoch_normal[trial,channel_index,start_index:stop_index], color = 'blue', label = 'normal')
+        else:
+            plt.plot(button_time_array, eeg_epoch_normal[trial,channel_index,start_index:stop_index], color = 'blue')
+
+    for trial in range(eeg_epoch_frustrated.shape[0] - 1):
+        # Isolate the right subset of the Y_data
+        trial_Y_data = Y_data[frustrated_epoch_masks[trial]]
+        
+        # Find time of button presses
+        button_presses = np.array(np.where(trial_Y_data==1) or np.where(trial_Y_data==2) or np.where(trial_Y_data==3) or np.where(trial_Y_data==4))
+        button_presses = button_presses.flatten()
+        
+        start_index=int(start_stop_time[0]/sampling_period) + button_presses[0] #compute integer index for start time
+        stop_index=int(start_stop_time[1]/sampling_period) + button_presses[0] #compute integer index for start time
+        time_array = epoch_time_array[start_index:stop_index]
+        eeg_data = eeg_epoch_normal[trial,channel_index,start_index:stop_index]
+        
+        if start_index < 0:
+            stop_index += abs(start_index)
+            start_index = 0
+        
+        if trial==0: 
+            plt.plot(button_time_array, eeg_epoch_frustrated[trial,channel_index,start_index:stop_index], color = 'red', label = 'frustrated')
+        else: 
+            plt.plot(button_time_array, eeg_epoch_frustrated[trial,channel_index,start_index:stop_index], color = 'red')
+
+    plt.legend()
+    channel = ''.join(channel_to_plot)
+    plt.grid()
+    plt.title(f"Channel {channel} Subject {subject} {start_stop_time[0]} to {start_stop_time[1]}s After First Button Press")
+    plt.savefig(f'plots/subject-{subject}_channel-{channel}_postpress')
+    plt.show()
--- test_affpac_data.py
+++ test_affpac_data.py
@@ -19,17 +19,21 @@ plt.close('all')
 
 #Build data file string
 data_directory='./BNCI_data/'
-for subject in [0, 1, 2, 4, 6, 7, 9, 10, 11]:
+for subject in [0]:  #[0, 1, 2, 4, 6, 7, 9, 10, 11]
 
     #%% Load and plot AffPac data
 
     channels, information_array, eeg_data, Y_data = afp.load_affpac_data(subject,data_directory)
 
-    #afp.plot_raw_data(data,subject, eeg_data, Y_data, information_array,channels,['Fz','Oz','F4'],[15,80])
+    afp.plot_raw_data(subject, eeg_data, Y_data, information_array, channels,['Oz'],[0,2000])
     # afp.plot_raw_data(subject, eeg_data, Y_data, information_array,channels,channels_to_plot=['Fz','Oz','F4'])
     #%% Epoch eeg data for normal and frustrated events (Marker==22,23,24,25)
 
     #Note: This function could be changed to include markers to epoch as an argument
-    eeg_epoch_normal, eeg_epoch_frustrated = afp.epoch_eeg_data(eeg_data,Y_data)
-
-    afp.plot_epoch_data(eeg_epoch_normal, eeg_epoch_frustrated, channels, subject, channel_to_plot=['Fz'])
+    eeg_epoch_normal, eeg_epoch_frustrated, normal_epoch_masks, frustrated_epoch_masks, epoch_time_array = afp.epoch_eeg_data(eeg_data, Y_data, information_array)
+    
+    # afp.plot_epoch_data(eeg_epoch_normal, eeg_epoch_frustrated, epoch_time_array, channels, subject, channel_to_plot=['Fz'])
+    
+    afp.plot_after_button_press(eeg_epoch_normal, eeg_epoch_frustrated, normal_epoch_masks, frustrated_epoch_masks, epoch_time_array, Y_data, channels, subject, channel_to_plot=["Fz"], start_stop_time=[-1, 2], presses="first", separate_by_side=False)
+    
+    
