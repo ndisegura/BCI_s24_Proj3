7b9ddccb442ee0c989ae9e634a6019d4176fdc1c
Piper Welch <pwelch1@uvm.edu>
2024-05-01 20:52:25 -0400

    add topo plots, add plot by epoch

--- load_affpac_data.py
+++ load_affpac_data.py
@@ -8,38 +8,63 @@ Dr. David Jangraw
 4/27/2024
 
 """
-
-
-#from loadmat import loadmat #Not sure where to find this module. I tried uses pip install loadmat and returned an error. Using scipy instead
-import scipy.io as sio
 import numpy as np
 import matplotlib.pyplot as plt
 import loadmat 
+import plot_topo
+
 
 def load_affpac_data(subject_id=0, data_path = 'data/'): 
     if subject_id < 10: data = loadmat.loadmat(f'{data_path}/S0{subject_id}')
     else: data = loadmat.loadmat(f'{data_path}/S{subject_id}')
-    # print(data.keys())
-    # quit()
+
     return data['chann'],data['I'],data['X'],data['Y']
 
-def plot_raw_data(subject, eeg_data, Y_data, information_array, channels, channels_to_plot,start_stop_time=[0,-1]):
+
+def plot_raw_data(subject, eeg_data, Y_data, information_array, channels, channels_to_plot,start_stop_time=[0,-1], is_plot_y=False, is_plot_epochs=True):
     
     eeg_time=information_array[0]
     sampling_period=eeg_time[1]-eeg_time[0]
-    fs=1/(sampling_period)
     start_index=int(start_stop_time[0]/sampling_period) #compute integer index for start time
     stop_index=int(start_stop_time[1]/sampling_period) #compute integer index for start time
 
     fig, axs = plt.subplots(2,sharex=True)
     fig.suptitle(f'AffPac Subject {subject} Raw Data')
-    
 
-    axs[0].plot(eeg_time[start_index:stop_index],np.squeeze(Y_data[start_index:stop_index]),label='Marker Events')
+    if is_plot_epochs: 
+        normal_epoch_index_start = np.where(Y_data == 22)[0]
+        normal_epoch_index_end = np.where(Y_data == 23)[0]
+
+        frustrated_epoch_index_start = np.where(Y_data == 24)[0]
+        frustrated_epoch_index_end = np.where(Y_data == 25)[0]
+        if normal_epoch_index_start.shape > normal_epoch_index_end.shape: 
+            normal_epoch_index_start = normal_epoch_index_start[:-1]
+        if frustrated_epoch_index_start.shape > frustrated_epoch_index_end.shape: 
+            frustrated_epoch_index_start = frustrated_epoch_index_start[:-1]
+
+        for trial in range(len(normal_epoch_index_end)):
+            if trial == 0: 
+                axs[0].plot([eeg_time[normal_epoch_index_start[trial]], eeg_time[normal_epoch_index_end[trial]]],[0]*2, color = "blue", label = "Normal")
+            else: 
+                axs[0].plot([eeg_time[normal_epoch_index_start[trial]], eeg_time[normal_epoch_index_end[trial]]],[0]*2, color = "blue")
+
+            axs[0].scatter(eeg_time[normal_epoch_index_end[trial]], [0], color = 'blue')
+            axs[0].scatter(eeg_time[normal_epoch_index_start[trial]], [0], color='blue')
+        for trial in range(len(frustrated_epoch_index_end)):
+            if trial == 0: 
+                axs[0].plot([eeg_time[frustrated_epoch_index_start[trial]], eeg_time[frustrated_epoch_index_end[trial]]],[0]*2, color = "red", label = "Frustrated")
+            else:
+                axs[0].plot([eeg_time[frustrated_epoch_index_start[trial]], eeg_time[frustrated_epoch_index_end[trial]]],[0]*2, color = "red")
+            axs[0].scatter(eeg_time[frustrated_epoch_index_end[trial]], [0], color = 'red')
+            axs[0].scatter(eeg_time[frustrated_epoch_index_start[trial]], [0], color='red')
+
+    if is_plot_y: 
+        axs[0].plot(eeg_time[start_index:stop_index],np.squeeze(Y_data[start_index:stop_index]),label='Marker Events')
     axs[0].set_ylabel('Marker Event')
     axs[0].set_xlabel('Time (s)')
     axs[0].grid()
-        
+    axs[0].legend()
+    
     #PLot EEG Data
     for channel_index, channel_member in enumerate(channels_to_plot):
         is_channel_match=channels==channel_member #Boolean indexing across rows for item in list
@@ -51,8 +76,9 @@ def plot_raw_data(subject, eeg_data, Y_data, information_array, channels, channe
     axs[1].legend()
     axs[1].grid()
     plt.tight_layout()
-    plt.show()
-    
+    plt.savefig(f"plots/subject{subject}_raw_data")
+    plt.close()
+ 
     
 def epoch_eeg_data(eeg_data,Y_data):
     
@@ -60,8 +86,8 @@ def epoch_eeg_data(eeg_data,Y_data):
     #and frustration (Code 24/25)
     #Determine indexes of start and end of epoch for normal
     normal_epoch_index_start = np.where(Y_data == 22)[0]
-
     normal_epoch_index_end = np.where(Y_data == 23)[0]
+
     frustrated_epoch_index_start = np.where(Y_data == 24)[0]
     frustrated_epoch_index_end = np.where(Y_data == 25)[0]
 
@@ -82,7 +108,7 @@ def epoch_eeg_data(eeg_data,Y_data):
 
     for epoch_index in range(epoch_count_normal-1):
         curr_epoch_length = normal_epoch_index_end[epoch_index] - normal_epoch_index_start[epoch_index]
-        eeg_epoch_normal[epoch_index, :,:curr_epoch_length]= eeg_data[:,normal_epoch_index_start[epoch_index]:normal_epoch_index_end[epoch_index]]
+        eeg_epoch_normal[epoch_index, :,:curr_epoch_length] = eeg_data[:,normal_epoch_index_start[epoch_index]:normal_epoch_index_end[epoch_index]]
 
     for epoch_index in range(epoch_count_frustrated-1):
         curr_epoch_length = frustrated_epoch_index_end[epoch_index] - frustrated_epoch_index_start[epoch_index]
@@ -91,22 +117,43 @@ def epoch_eeg_data(eeg_data,Y_data):
     return eeg_epoch_normal, eeg_epoch_frustrated
 
 
-def plot_epoch_data(eeg_epoch_normal, eeg_epoch_frustrated, channels, subject, channel_to_plot): 
+def plot_epoch_data(eeg_epoch_normal, eeg_epoch_frustrated, channels, subject, channel_to_plot, fs = 128): 
     channel_index = np.where(channels==channel_to_plot)[0][0]
+
+    time = np.linspace(0, eeg_epoch_frustrated.shape[2]*(1/128), eeg_epoch_frustrated.shape[2])
     for trial in range(eeg_epoch_normal.shape[0]):
         if trial==0: 
-            plt.plot(eeg_epoch_normal[trial,channel_index,:], color = 'blue', label = 'normal')
+            plt.plot(time, eeg_epoch_normal[trial,channel_index,:], color = 'blue', label = 'normal')
         else:
-            plt.plot(eeg_epoch_normal[trial,channel_index,:], color = 'blue')
+            plt.plot(time, eeg_epoch_normal[trial,channel_index,:], color = 'blue')
 
     for trial in range(eeg_epoch_frustrated.shape[0]):
-        if trial==0: 
-            plt.plot(eeg_epoch_frustrated[trial,channel_index,:], color = 'red', label = 'frustrated')
+        if trial==0:
+            plt.plot(time, eeg_epoch_frustrated[trial,channel_index,:], color = 'red', label = 'frustrated')
         else: 
-            plt.plot(eeg_epoch_frustrated[trial,channel_index,:], color = 'red')
+            plt.plot(time, eeg_epoch_frustrated[trial,channel_index,:], color = 'red')
 
     plt.legend()
     channel = ''.join(channel_to_plot)
     plt.title(f"Channel {channel} Subject {subject}")
+    plt.grid(True)
+    plt.ylabel("Time (s)")
     plt.savefig(f'plots/subject{subject}_channel{channel}')
+    plt.close()
+
+
+def plot_topographic(subject, eeg_epoch_normal, eeg_epoch_frustrated, channels, channels_to_plot):
+    fig, axes = plt.subplots(1, 2, figsize=(6,4))
+    axes = axes.flatten()
+
+    channel_indices = np.arange(0, 32, 1)
+
+    channel_data1 = np.nanmedian(eeg_epoch_normal[:,channel_indices,:], axis=0)
+    plot_topo.plot_topo(axes = axes[0], channel_names = list(channels[:32]), channel_data=channel_data1, title="Normal")
+
+    channel_data = np.nanmedian(eeg_epoch_frustrated[:,channel_indices,:], axis=0)
+    plot_topo.plot_topo(axes = axes[1], channel_names = list(channels[:32]), channel_data=channel_data, title="Frustrated")
+    
+    plt.suptitle(f"Subject {subject}")
+    plt.savefig(f"plots/plot_topo_subject{subject}")
     plt.close()
--- test_affpac_data.py
+++ test_affpac_data.py
@@ -20,16 +20,17 @@ plt.close('all')
 #Build data file string
 data_directory='./BNCI_data/'
 for subject in [0, 1, 2, 4, 6, 7, 9, 10, 11]:
-
+    # subject = 0 
     #%% Load and plot AffPac data
 
     channels, information_array, eeg_data, Y_data = afp.load_affpac_data(subject,data_directory)
 
-    #afp.plot_raw_data(data,subject, eeg_data, Y_data, information_array,channels,['Fz','Oz','F4'],[15,80])
-    # afp.plot_raw_data(subject, eeg_data, Y_data, information_array,channels,channels_to_plot=['Fz','Oz','F4'])
+    afp.plot_raw_data(subject, eeg_data, Y_data, information_array,channels,channels_to_plot=['Fz','Oz','F4'])
     #%% Epoch eeg data for normal and frustrated events (Marker==22,23,24,25)
 
     #Note: This function could be changed to include markers to epoch as an argument
     eeg_epoch_normal, eeg_epoch_frustrated = afp.epoch_eeg_data(eeg_data,Y_data)
 
-    afp.plot_epoch_data(eeg_epoch_normal, eeg_epoch_frustrated, channels, subject, channel_to_plot=['Fz'])
+    afp.plot_epoch_data(eeg_epoch_normal, eeg_epoch_frustrated, channels, subject, channel_to_plot=['Oz'])
+
+    afp.plot_topographic(subject, eeg_epoch_normal, eeg_epoch_frustrated, channels, ['Fz', 'Oz'])
